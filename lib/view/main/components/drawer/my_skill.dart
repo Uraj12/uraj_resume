c3aa247b6abf768a1d21cc337da6214595b6212c
sahu uraj <139441319+Uraj12@users.noreply.github.com>
2025-04-15 18:34:16 +0530

    init myskill

--- lib/view/main/components/drawer/my_skill.dart
+++ lib/view/main/components/drawer/my_skill.dart
@@ -4,64 +4,108 @@ import '../../../../res/constants.dart';
 import '../../../intro/components/commonText.dart';
 
 class AnimatedLinearProgressIndicator extends StatelessWidget {
-  const AnimatedLinearProgressIndicator({super.key, required this.percentage, required this.title, this.image});
+  const AnimatedLinearProgressIndicator({
+    super.key,
+    required this.percentage,
+    required this.title,
+    this.image,
+  });
+
   final double percentage;
   final String title;
   final String? image;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  const EdgeInsets.only(bottom: defaultPadding/2),
-      child: TweenAnimationBuilder(tween: Tween(begin: 0.0,end: percentage), duration: const Duration(seconds: 1), builder: (context, value, child) {
-        return Column(
-          children: [
-            Row(
-              children: [
-                Image.asset(image!,height: 15,width: 15,fit: BoxFit.cover,),
-                const SizedBox(width: 5,),
-                Text(title,style: const TextStyle(color: Colors.white),),
-                const Spacer(),
-                Text('${(value*100).toInt().toString()}%'),
-              ],
-            ),
-            const SizedBox(height: defaultPadding/2,),
-            LinearProgressIndicator(
-              value: value,
-              backgroundColor: Colors.black,
-              color: Colors.amberAccent,
-            ),
-          ],
-        );
-      },),
+      padding: const EdgeInsets.only(bottom: defaultPadding / 2),
+      child: TweenAnimationBuilder(
+        tween: Tween(begin: 0.0, end: percentage),
+        duration: const Duration(seconds: 1),
+        builder: (context, value, child) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  if (image != null)
+                    Image.asset(
+                      image!,
+                      height: 15,
+                      width: 15,
+                      fit: BoxFit.cover,
+                    ),
+                  const SizedBox(width: 5),
+                  Text(
+                    title,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  const Spacer(),
+                  Text('${(value * 100).toInt()}%'),
+                ],
+              ),
+              const SizedBox(height: defaultPadding / 2),
+              LinearProgressIndicator(
+                value: value,
+                backgroundColor: Colors.black,
+                color: Colors.amberAccent,
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
 
-class MySKills extends StatelessWidget {
-  const MySKills({super.key});
+class MySkills extends StatelessWidget {
+  const MySkills({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SkillWithIcon(title: "Flutter", image: 'assets/icons/flutter.png'),
-        SkillWithIcon(title: "Dart", image: 'assets/icons/dart.png'),
-        SkillWithIcon(title: "Firebase", image: 'assets/icons/firebase.png'),
-        SkillWithIcon(title: "Mysql", image: 'assets/icons/dart.png'),
-        SkillWithIcon(title: "Responsive Design", image: 'assets/icons/flutter.png'),
-        // SkillWithIcon(title: "Advance Java", image: 'assets/icons/java.png'),
-        SkillWithIcon(title: "Python", image: 'assets/icons/bloc.png'),
-        SkillWithIcon(title: "Spring Boot", image: 'assets/icons/dart.png'),
-
-        // AnimatedLinearProgressIndicator(percentage: 0.7, title: 'Flutter',image: 'assets/icons/flutter.png',),
-        // AnimatedLinearProgressIndicator(percentage: 0.9, title: 'Dart',image: 'assets/icons/dart.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.6, title: 'Firebase',image: 'assets/icons/firebase.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.85, title: 'Mysql',image: 'assets/icons/dart.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.8, title: 'Responsive Design',image: 'assets/icons/flutter.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.9, title: 'Advance java',image: 'assets/icons/flutter.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.5, title: 'python',image: 'assets/icons/bloc.png'),
-        // AnimatedLinearProgressIndicator(percentage: 0.93, title: 'Spring boot',image: 'assets/icons/dart.png'),
-      ],);
+        AnimatedLinearProgressIndicator(
+          percentage: 0.5,
+          title: 'Flutter',
+          image: 'assets/icons/flutter.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.6,
+          title: 'Dart',
+          image: 'assets/icons/dart.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.7,
+          title: 'Firebase',
+          image: 'assets/icons/firebase.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.85,
+          title: 'MySQL',
+          image: 'assets/icons/dart.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.8,
+          title: 'Responsive Design',
+          image: 'assets/icons/flutter.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.9,
+          title: 'Advance Java',
+          image: 'assets/icons/flutter.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.5,
+          title: 'Python',
+          image: 'assets/icons/bloc.png',
+        ),
+        AnimatedLinearProgressIndicator(
+          percentage: 0.93,
+          title: 'Spring Boot',
+          image: 'assets/icons/dart.png',
+        ),
+      ],
+    );
   }
 }
